7a7b9810374f7b46aa320dd79bc334b8120b102f
drushti <drushtigowda1@gmail.com>
2025-04-22 20:13:22 +0530

    Add files via upload

---
+++ arithmetic_demo.java
@@ -0,0 +1,19 @@
+class arithmetic_demo
+{
+	public static void main(String args[])
+	{
+		int x=100,y=90;
+		int sum;
+		sum=x+y;
+		System.out.println("sum="+sum);
+		int sub;
+		sub=x-y;
+		System.out.println("sub="+sub);
+		double div;
+		div=x/y;
+		System.out.println("div="+div);
+		int mul;
+		mul=x*y;
+		System.out.println("mul="+mul);
+	}
+}
